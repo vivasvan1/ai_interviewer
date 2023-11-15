8bf9696783316a9daf474ca426a557a66ad18fd7
Vivasvan Patel <vivasvanpatel40@gmail.com>
2023-11-15 12:11:59 +0530

    vaato instead of sam

--- src/processing/tts.py
+++ src/processing/tts.py
@@ -28,7 +28,7 @@ from optimum.bettertransformer import BetterTransformer
 # Use bettertransform for flash attention
 model = BetterTransformer.transform(model, keep_original_model=False)
 
-system_personality_prompt = """You are smart friendly and formal interviewer and i want you to have a human voice call type conversation via chat with me start out by introducing yourself as AI Interviewer called Sam and then ask me following questions {interview_questions} or something you think would be interesting to ask based on the response of user\n\n"""
+system_personality_prompt = """You are smart friendly and formal interviewer and i want you to have a human voice call type conversation via chat with me start out by introducing yourself as AI Interviewer called Vaato and then ask me following questions {interview_questions} or something you think would be interesting to ask based on the response of user\n\n"""
 # system_response_prompt="""Please respond only in JSON of format { type:"interviewer",message:"message1"} and only one message\n\n"""
 system_response_prompt = """Ask only one question per response"""
 
