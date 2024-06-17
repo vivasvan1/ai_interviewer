aee2e13a4386a3fe6d8d4b12634b0de71fce456b
Vivasvan Patel <vivasvanpatel40@gmail.com>
2024-06-17 14:39:58 +0530

    adding /questions

--- src/processing/resume.py
+++ src/processing/resume.py
@@ -1,12 +1,28 @@
 from logging import debug, info
 import logging
 import shutil
+from typing import List
 import openai
 from pypdf import PdfReader
 import tempfile
 import fitz
 
+from abc import ABC, abstractmethod
+
 # from src.processing.tts import reset_llms
+
+
+def read_pdf(upload_file: str) -> str:
+    text = ""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        # Save uploaded file to a temporary file
+        shutil.copyfileobj(upload_file, temp_file)
+        temp_file_path = temp_file.name
+    # Read PDF from the temporary file
+    doc = fitz.open(temp_file_path)
+    for page in doc:  # iterate the document pages
+        text += page.get_text()  # get plain text encoded as UTF-8
+    return text
 
 
 def get_questions_from_file(text: str, document_name: str):
@@ -26,7 +42,6 @@ def get_questions_from_file(text: str, document_name: str):
         ],
     )
     questions_text = ""
-    questions_json = ""
 
     # Parse the 'choices' field from the API response
     if "choices" in api_response and len(api_response["choices"]) > 0:
@@ -38,19 +53,6 @@ def get_questions_from_file(text: str, document_name: str):
         debug("Failed to get a valid response.")
 
     return questions_text
-
-
-def read_pdf(upload_file: str) -> str:
-    text = ""
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        # Save uploaded file to a temporary file
-        shutil.copyfileobj(upload_file, temp_file)
-        temp_file_path = temp_file.name
-    # Read PDF from the temporary file
-    doc = fitz.open(temp_file_path)
-    for page in doc:  # iterate the document pages
-        text += page.get_text()  # get plain text encoded as UTF-8
-    return text
 
 
 def get_questions_from_resume_and_jd(resume_text: str, jd_text: str):
@@ -88,7 +90,54 @@ def get_questions_from_resume_and_jd(resume_text: str, jd_text: str):
     return questions_text
 
 
-basic_questions = """Can you tell me about yourself?
+def get_questions_from_resume_and_pre_generated_questions(
+    resume_text: str, questions_list: List[str]
+):
+    # # Read Resume
+    # resume_text = read_pdf(path_to_resume)
+    # # Read JD
+    # jd_text = read_pdf(path_to_jd)
+
+    # Get the questions from GPT-3
+    pre_generated_questions = "\n".join(questions_list)
+    api_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an interviewer and given some pre generated questions and resume and i want you to provide me a list of modified questions so that they are more relevant to the resume. ",
+            },
+            {
+                "role": "user",
+                "content": f"Pre Generated Questions: {pre_generated_questions} and My Resume: {resume_text}",
+            },
+        ],
+    )
+    questions_text = ""
+    questions_json = ""
+
+    # Parse the 'choices' field from the API response
+    if "choices" in api_response and len(api_response["choices"]) > 0:
+        # Get the 'text' field from the first choice
+        questions_text = api_response["choices"][0]["message"]["content"]
+        debug(f"GPT-3 Response: {questions_text}")
+        # questions_json = json.loads(questions_text)
+    else:
+        debug("Failed to get a valid response.")
+
+    return questions_text
+
+
+class QuestionGenerationStrategy(ABC):
+
+    @abstractmethod
+    def generate(self, resume_text, jd_text, questions_list):
+        pass
+
+
+class DefaultQuestionStrategy(QuestionGenerationStrategy):
+    def generate(self, resume_text, jd_text, questions_list):
+        basic_questions = """Can you tell me about yourself?
 What are your strengths and weaknesses?
 Why do you want to work for this company?
 Can you describe a challenging situation you've faced at work and how you handled it?
@@ -98,75 +147,165 @@ What is your greatest professional achievement?
 What is your preferred work style or work environment?
 Why should we hire you for this position?
 Do you have any questions for us?"""
+        return basic_questions
+
+
+class ResumeBasedQuestionStrategy(QuestionGenerationStrategy):
+    def generate(self, resume_text, jd_text, questions_list):
+        # Logic for generating questions based solely on resume text
+        gen_question_text = get_questions_from_file(resume_text, "resume")
+
+        return gen_question_text
+
+
+class JDBasedQuestionStrategy(QuestionGenerationStrategy):
+
+    def generate(self, resume_text, jd_text, questions_list):
+        # Logic for generating questions based solely on job description text
+        gen_question_text = get_questions_from_file(jd_text, "interview context")
+
+        return gen_question_text
+
+
+class CombinedResumeWithJobDescStrategy(QuestionGenerationStrategy):
+
+    def generate(self, resume_text, jd_text, questions_list):
+        # Logic for generating questions based on both resume and job description texts
+        gen_question_text = get_questions_from_resume_and_jd(resume_text, jd_text)
+        return gen_question_text
+
+
+class CombinedResumeWithQuestionsStrategy(QuestionGenerationStrategy):
+    def generate(self, resume_text, jd_text, questions_list):
+        # Logic for generating questions based on both resume and job description texts
+        gen_question_text = get_questions_from_resume_and_pre_generated_questions(
+            resume_text, questions_list
+        )
+        return gen_question_text
+
+
+class QuestionListStrategy(QuestionGenerationStrategy):
+
+    def generate(self, resume_text, jd_text, questions_list):
+        # Logic for generating questions based on both resume and job description texts
+        gen_question_text = "\n".join(questions_list)
+        return gen_question_text
+
+
+def combine_file_content_and_text(file_path, text):
+    """Combine the content of a PDF file with a given text
+
+    Args:
+        file_path (str): Path to the PDF file
+        text (str): Any text to be added to the output
+
+    Returns:
+        str: The combined content of the PDF file and the given text
+    """
+    combined = ""
+    if file_path:
+        extracted_text = read_pdf(file_path)
+        combined += extracted_text + "\n\n"
+    if text:
+        combined += text + "\n\n"
+    return combined
+
+
+def calculate_questions(
+    resume_file=None,
+    jd_file=None,
+    resume_text=None,
+    jd_text=None,
+    questions=None,
+    questions_list=[],
+    is_dynamic=True,
+):
+    # Combine resume_text and resume_file if any one or both available
+    full_resume_text = combine_file_content_and_text(resume_file, resume_text)
+
+    # Combine jd_text and jd_file if any one or both available
+    full_jd_text = combine_file_content_and_text(jd_file, jd_text)
+
+    # Determine which strategy to use based on the inputs
+    strategy = DefaultQuestionStrategy()
+    if is_dynamic:
+        if len(questions_list) > 0 and full_resume_text:
+            strategy = CombinedResumeWithQuestionsStrategy()
+        elif len(questions_list) > 0:
+            strategy = QuestionListStrategy()
+        elif full_resume_text and full_jd_text:
+            strategy = CombinedResumeWithJobDescStrategy()
+        elif full_resume_text:
+            strategy = ResumeBasedQuestionStrategy()
+        elif full_jd_text:
+            strategy = JDBasedQuestionStrategy()
+    else:
+        if len(questions_list) > 0:
+            strategy = QuestionListStrategy()
+
+    print("strategy", strategy)
+
+    gen_question_text = strategy.generate(
+        full_resume_text, full_jd_text, questions_list
+    )
+
+    # Combine ProvidedQuestions with GeneratedQuestions
+    final_questions = (
+        (gen_question_text + "\n" + questions) if questions else gen_question_text
+    )
+
+    print("final_questions", final_questions)
+
+    return final_questions
 
 
 def process_resume_and_jd(
-    resume_file=None, jd_file=None, resume_text=None, jd_text=None, questions=None
+    resume_file=None,
+    jd_file=None,
+    resume_text=None,
+    jd_text=None,
+    questions=None,
+    questions_list=[],
+    is_dynamic=True,
+    voice="alloy",
 ):
-    final_questions = ""
-
-    # No info was provided set question to default
-    if (
-        resume_file == None
-        and resume_text == None
-        and jd_file == None
-        and jd_text == None
-        and questions == None
-    ):
-        # Load Default Questions
-        final_questions = basic_questions
-
-    # Case: Some context was provided
-    else:
-        # Combine resume_text and resume_file if any one or both available
-        full_resume_text = ""
-        if resume_file:
-            extracted_text = read_pdf(resume_file)
-            full_resume_text += extracted_text + "\n\n"
-        if resume_text:
-            full_resume_text += resume_text + "\n\n"
-
-        # Combine jd_text and jd_file if any one or both available
-        full_jd_text = ""
-        if jd_file:
-            extracted_text = read_pdf(jd_file)
-            full_jd_text += extracted_text + "\n\n"
-        if jd_text:
-            full_jd_text += jd_text + "\n\n"
-
-        gen_question_text = ""
-        if (not full_resume_text) and (not full_jd_text):
-            # Direct Questions Provided.
-            pass
-        elif full_resume_text and (not full_jd_text):
-            # Generate Question from Resume.
-            gen_question_text = get_questions_from_file(full_resume_text, "resume")
-        elif full_jd_text and (not full_resume_text):
-            # Generate Question from JD.
-            gen_question_text = get_questions_from_file(full_jd_text, "interview context")
-        else:
-            # Generate Question from Resume + JD.
-            gen_question_text = get_questions_from_resume_and_jd(
-                full_resume_text, full_jd_text
-            )
-
-        # Combine ProvidedQuestions with GeneratedQuestions
-        final_questions = (
-            (questions + " and " + gen_question_text)
-            if questions and gen_question_text
-            else (questions if not gen_question_text else gen_question_text)
+    print(
+        "process_resume_and_jd",
+        resume_file,
+        jd_file,
+        resume_text,
+        jd_text,
+        questions,
+        questions_list,
+        is_dynamic,
+    )
+    final_questions = calculate_questions(
+        resume_file,
+        jd_file,
+        resume_text,
+        jd_text,
+        questions,
+        questions_list,
+        is_dynamic,
+    )
+    debug("final_questions : " + str(final_questions))
+    system_personality_prompt = (
+        f"""You are smart friendly and formal interviewer and I want you to have a human voice call type conversation via chat with me ask me following questions {final_questions} or something you think would be interesting to ask based on the response of user."""
+        + (
+            "You may also ask follow up question if needed based on user's response"
+            if is_dynamic
+            else ""
         )
-    debug("final_questions : "+ str(final_questions))
-    system_personality_prompt = """You are smart friendly and formal interviewer and i want you to have a human voice call type conversation via chat with me ask me following questions {interview_questions} or something you think would be interesting to ask based on the response of user\n\n"""
-    # system_response_prompt="""Please respond only in JSON of format { type:"interviewer",message:"message1"} and only one message\n\n"""
+        + "\n\n"
+    )
+    print("system_personality_prompt", system_personality_prompt)
     system_response_prompt = """Ask only one question per response"""
     system_message = system_personality_prompt + system_response_prompt
 
-    system_message = system_message.replace("{interview_questions}", final_questions)
-
     # out = chat(chat_messages)
     # ai_reply = json.loads(out.content)["message"]
-    ai_reply: str= "" # just kept for historical purpose
-    # ai_reply: str = f"Hi there this is {voice}. Your AI Interviewer for today. Hope you are doing well. Shall we get started?"
-    # chat_messages.append(AIMessage(content=ai_reply))
+    ai_reply: str = (
+        f"Hi there, this is {('Sam' if voice == 'alloy' else voice.capitalize())}. Your AI interviewer for today. Hope you are doing well. Shall we get started?"
+    )
+
     return ai_reply, final_questions, system_message
--- src/routes/interview/interview.py
+++ src/routes/interview/interview.py
@@ -1,14 +1,14 @@
-
 import logging
 import traceback
 from langchain.schema import SystemMessage
 from src.utils.audio import convert_audio_to_base64
 from src.agent.simple import process_user_response
 from src.history.ChatMessageHistory import ChatMessageHistoryWithJSON
-from src.processing.resume import process_resume_and_jd
+from src.processing.resume import calculate_questions, process_resume_and_jd
 from src.processing.tts import do_text_to_speech
 from fastapi import APIRouter, Body, FastAPI, UploadFile, Form, File, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 
 class AIResponse(BaseModel):
@@ -16,8 +16,39 @@ class AIResponse(BaseModel):
     history: str
 
 
+class QuestionsResponse(BaseModel):
+    questions: List[str]
+
+
 router = APIRouter()
 
+
+@router.post(
+    "/interview/questions",
+    response_model=QuestionsResponse,
+    tags=["Interview"],
+    description="Get questions for the interview",
+)
+async def get_questions(
+    resume: UploadFile = None,
+    resumeText: str = Body(default=None),
+    jd: UploadFile = None,
+    jdText: str = Body(default=None),
+    questions: str = Body(default=None),
+):
+    try:
+        question_text = calculate_questions(
+            resume.file if resume else None,
+            jd.file if jd else None,
+            resumeText,
+            jdText,
+            questions,
+            [],
+            True,
+        )
+        return {"questions": question_text.split("\n")}
+    except Exception as e:
+        raise HTTPException(detail=str(e.with_traceback()), status_code=400)
 
 
 @router.post(
@@ -27,29 +58,38 @@ router = APIRouter()
     description="Process the uploaded resume (optionally a job description) and produce AI response",
 )
 async def process_resume(
-    resume: UploadFile = None,resumeText: str = Body(default=None), jd: UploadFile = None,jdText: str = Body(default=None), questions: str = Body(default=None),voice: str = Body(default="alloy"),
+    resume: UploadFile = None,
+    resumeText: str = Body(default=None),
+    jd: UploadFile = None,
+    jdText: str = Body(default=None),
+    questions: str = Body(default=None),
+    questions_list: List[str] = Body(default=[]),
+    is_dynamic: bool = Body(default=True),
+    voice: str = Body(default="alloy"),
 ):
-    try:    
+    try:
         ai_reply, question_text, system_message = process_resume_and_jd(
-            resume.file if resume else None, 
+            resume.file if resume else None,
             jd.file if jd else None,
             resumeText,
             jdText,
-            questions
+            questions,
+            questions_list[0].split(",") if len(questions_list) else questions_list,
+            is_dynamic,
         )
         history = ChatMessageHistoryWithJSON()
         history.add_message(SystemMessage(content=system_message))
         history.add_ai_message(ai_reply)
 
         # ai_response_base64 = convert_audio_to_base64(do_text_to_speech(ai_reply , voice=voice))
-        
+
         # # save ai_response_base64 to file
         # with open(f'./public/first_messages/ai_first_reply_{voice}.wav', 'wb') as f:
         #     f.write(ai_response_base64.encode('ascii'))
-        
-        with open(f'./public/first_messages/ai_first_reply_{voice}.wav', 'rb') as f:
-            ai_response_base64 = f.read().decode('ascii')
-        
+
+        with open(f"./public/first_messages/ai_first_reply_{voice}.wav", "rb") as f:
+            ai_response_base64 = f.read().decode("ascii")
+
         return {"response": ai_response_base64, "history": history.to_json()}
 
     except Exception as e:
@@ -63,7 +103,9 @@ async def process_resume(
     description="Process user's audio response and compute AI response",
 )
 async def user_response(
-    response_audio: UploadFile = File(...), chat_messages: str = Form(...),voice: str = Body(default="alloy"),
+    response_audio: UploadFile = File(...),
+    chat_messages: str = Form(...),
+    voice: str = Body(default="alloy"),
 ):
     try:
         logging.info("Received request for /interview/next")
@@ -82,7 +124,9 @@ async def user_response(
         if not ai_reply:
             raise ValueError("AI reply is empty or null")
 
-        ai_response_base64 = convert_audio_to_base64(do_text_to_speech(ai_reply,voice=voice))
+        ai_response_base64 = convert_audio_to_base64(
+            do_text_to_speech(ai_reply, voice=voice)
+        )
         if not ai_response_base64:
             raise ValueError("Failed to convert AI reply to base64 encoded audio")
         return {"response": ai_response_base64, "history": history.to_json()}
